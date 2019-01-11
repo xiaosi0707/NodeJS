2cda0c4cd1c02fdc2296718816b9eb65015ed681
wyunfei <583514368@qq.com>
2019-01-11 10:36:02 +0800

    从json 文件读取数据

--- crud/app.js
+++ crud/app.js
@@ -5,6 +5,7 @@
  */
 let express = require('express')
 let app = express()
+let fs = require('fs')
 
 app.engine('html', require('express-art-template'))
 
@@ -15,13 +16,17 @@ app.use('/public/', express.static('./public'))
 
 
 app.get('/', (req, res) => {
-    res.render('index.html', {
-        students: [
-            '孙权',
-            '曹操',
-            '刘备'
-        ]
+    // readFile第二个参数是可选的，传入utf-8 就是告诉它把读取到的文件直接按照utf-8编码转成我们能认识的字符
+    // 还可以通过data.toString()的方式进行转换
+    fs.readFile('db.json', 'utf-8', (err, data) => {
+        if (err) return res.status(500).send('Server error')
+        console.log(JSON.parse(data))
+        res.render('index.html', {
+            students: JSON.parse(data).students
+        })
+
     })
+
 })
 
 app.listen(7000, () => {
