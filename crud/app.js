261e2409b55d8642eff43136f3928a8a257f49b0
wyunfei <583514368@qq.com>
2019-01-11 09:50:12 +0800

    模板制作

--- crud/app.js
+++ crud/app.js
@@ -8,10 +8,20 @@ let app = express()
 
 app.engine('html', require('express-art-template'))
 
+// 设置开放目录
+app.use('/node_modules/', express.static('./node_modules/'))
+app.use('/public/', express.static('./public'))
+
 
 
 app.get('/', (req, res) => {
-    res.render('index.html')
+    res.render('index.html', {
+        students: [
+            '孙权',
+            '曹操',
+            '刘备'
+        ]
+    })
 })
 
 app.listen(7000, () => {
