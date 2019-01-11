f6dc851240710ede506728eaf991a4d86c5c4d36
wyunfei <583514368@qq.com>
2019-01-11 14:35:10 +0800

    router封装第二种方式 - Express

--- crud/app.js
+++ crud/app.js
@@ -2,13 +2,24 @@
  @Author：Wyunfei
  @Date：2019/1/10/16:38
  @FileName: app.js
+    职责：
+    启动服务
+    做一些服务相关配置
+        模板引擎
+        body-parser
+        静态资源服务
+        监听端口
+        挂载路由
  */
 let express = require('express')
 let app = express()
 let router = require('./router.js')
 
+// 4、把路由容器挂载到 app 服务中
+app.use(router)
+
 app.engine('html', require('express-art-template'))
-router(app)
+
 // 设置开放目录
 app.use('/node_modules/', express.static('./node_modules/'))
 app.use('/public/', express.static('./public'))
--- crud/router.js
+++ crud/router.js
@@ -4,30 +4,39 @@
  @FileName: router.js
  */
 let fs = require('fs')
-module.exports = (app) => {
-    app.get('/students', (req, res) => {
-        // readFile第二个参数是可选的，传入utf-8 就是告诉它把读取到的文件直接按照utf-8编码转成我们能认识的字符
-        // 还可以通过data.toString()的方式进行转换
-        fs.readFile('db.json', 'utf-8', (err, data) => {
-            if (err) return res.status(500).send('Server error')
-            res.render('index.html', {
-                students: JSON.parse(data).students
-            })
+
+// 专门用来包装路由的
+let express = require('express')
+
+// 1、创建一个路由容器
+let router = express.Router()
+
+// 2、把路由都挂载到 router 路由容器中
+router.get('/students', (req, res) => {
+    // readFile第二个参数是可选的，传入utf-8 就是告诉它把读取到的文件直接按照utf-8编码转成我们能认识的字符
+    // 还可以通过data.toString()的方式进行转换
+    fs.readFile('db.json', 'utf-8', (err, data) => {
+        if (err) return res.status(500).send('Server error')
+        res.render('index.html', {
+            students: JSON.parse(data).students
         })
     })
-    app.get('/students/new', (req, res) => {
+})
+router.get('/students/new', (req, res) => {
 
-    })
-    app.post('/students/new', (req, res) => {
+})
+router.post('/students/new', (req, res) => {
 
-    })
-    app.get('/', (req, res) => {
+})
+router.get('/', (req, res) => {
 
-    })
-    app.get('/', (req, res) => {
+})
+router.get('/', (req, res) => {
 
-    })
-    app.get('/', (req, res) => {
+})
+router.get('/', (req, res) => {
 
-    })
-}
+})
+
+// 3 导出router
+module.exports = router
