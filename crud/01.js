24456de8f5cac0e61148e79bfddb7969ddadcdd4
wyunfei <583514368@qq.com>
2019-02-03 10:41:52 +0800

    不知道做了什么

--- crud/01.js
+++ crud/01.js
@@ -3,40 +3,22 @@
  @Date：2019/1/11/15:32
  @FileName: 01.js
  */
-/*
-* 调用下面的 fn() 方法取到 return data 的值，该怎么做？
-* */
-
-// function fn() {
-//     // 这种？ - NO
-//     // setTimeout(() => {
-//     //     let data = 'hello'
-//     //     return data
-//     // }, 1000)
-//
-//     // 这种？ -- NO
-//     // let data = '默认数据'
-//     // setTimeout(() => {
-//     //     data = 'hello'
-//     // }, 1000)
-//     // return data
-//
-//     /*
-//     * 怎么办？
-//     * */
-// }
-// console.log(fn())
 
 
-// 凡是想要得到方法内部的异步操作结果，则必须通过回调函数来获取
-function fn(callback) {
-    setTimeout(() => {
-        let data = 'hello'
-        callback(data)
-    }, 1000)
+function get(url, callback) {
+    let oReq = new XMLHttpRequest()
+
+    // 当请求成功之后，调用回调函数
+    oReq.onload = () => {
+        // 我们需要通过回调函数的方式得到 oReq.responseText
+        callback(oReq.responseText)
+    }
+
+    oReq.open('get', url, true)
+    oReq.send()
 }
 
-fn((data) => {
+get('db.json', (data) => {
     console.log(data)
 })
 
